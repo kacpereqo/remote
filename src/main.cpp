5de5be557742a4764f4a139ba9f46087d376a1f5
kacpereqo <remzak.k@gmail.com>
2024-05-16 20:43:54 +0200

    remote

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,33 @@
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <RadioLib.h>
 
-// nRF24 has the following connections:
-// CS pin:    10
-// IRQ pin:   2
-// CE pin:    3
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 nRF24 radio = new Module(10, 2, 3);
-
-// or using RadioShield
-// https://github.com/jgromes/RadioShield
-// nRF24 radio = RadioShield.ModuleA;
-
-void setFlag();
 
 void setup()
 {
-  Serial.begin(115200);
 
-  // initialize nRF24 with default settings
+  Serial.begin(115200);
+  while (!Serial)
+    ;
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+  {
+    Serial.println(F("SSD1306 allocation failed"));
+    while (true)
+      ;
+  }
+
   Serial.print(F("[nRF24] Initializing ... "));
   int state = radio.begin();
   if (state == RADIOLIB_ERR_NONE)
@@ -31,10 +42,6 @@ void setup()
       ;
   }
 
-  // set transmit address
-  // NOTE: address width in bytes MUST be equal to the
-  //       width set in begin() or setAddressWidth()
-  //       methods (5 by default)
   byte addr[] = {0x01, 0x23, 0x45, 0x67, 0x89};
   Serial.print(F("[nRF24] Setting transmit pipe ... "));
   state = radio.setTransmitPipe(addr);
@@ -51,51 +58,73 @@ void setup()
   }
 }
 
-// counter to keep track of transmitted packets
-int count = 0;
-
 bool switch1 = false;
 bool switch2 = false;
 bool switch3 = false;
 bool switch4 = false;
 
-#define SWITCH1 A3
-#define SWITCH2 A0
-#define SWITCH3 6
-#define SWITCH4 7
+constexpr int SWITCH1 = A0;
+constexpr int SWITCH2 = A3;
+constexpr int SWITCH3 = 6;
+constexpr int SWITCH4 = 7;
 
 void loop()
 {
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(24, 12);
+  display.cp437(true);
+
+  Serial.print(F("[nRF24] Transmitting packet ... "));
+
   switch1 = digitalRead(SWITCH1);
   switch2 = digitalRead(SWITCH2);
   switch3 = digitalRead(SWITCH3);
   switch4 = digitalRead(SWITCH4);
 
-  Serial.print(F("[nRF24] Transmitting packet ... "));
-
-  String str = String(switch4) + String(switch3) + String(switch2) + String(switch1);
+  String str = String(switch4) + String(switch3) + String(switch1) + String(switch2);
   int state = radio.transmit(str);
 
   if (state == RADIOLIB_ERR_NONE)
   {
+    // the packet was successfully transmitted
     Serial.println(F("success!"));
   }
   else if (state == RADIOLIB_ERR_PACKET_TOO_LONG)
   {
+    // the supplied packet was longer than 32 bytes
     Serial.println(F("too long!"));
   }
   else if (state == RADIOLIB_ERR_ACK_NOT_RECEIVED)
   {
+    // acknowledge from destination module
+    // was not received within 15 retries
+    display.println("Connection NIE OK");
     Serial.println(F("ACK not received!"));
   }
   else if (state == RADIOLIB_ERR_TX_TIMEOUT)
   {
+    // timed out while transmitting
     Serial.println(F("timeout!"));
   }
   else
   {
+    // some other error occurred
     Serial.print(F("failed, code "));
     Serial.println(state);
   }
-  delay(100);
+
+  if (state != RADIOLIB_ERR_ACK_NOT_RECEIVED)
+  {
+    display.println("Connection OK");
+  }
+
+  display.setCursor(50, 24);
+  display.print(str);
+
+  display.display();
+
+  delay(1000 / 200);
 }
